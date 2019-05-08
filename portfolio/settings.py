09711b5e2b968178a2f8944af1315dc56759fc98
Abby Demarest <abbyjoydemarest@gmail.com>
2019-05-08 15:01:35 -0400

    installed django-heroku and adjusted settings appropriately

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
+BASE_DIR = os.path.portfolio(os.path.portfolio(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
