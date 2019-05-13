0af45eae89077d1849f7e20013e875aba2d1cae7
Abby Demarest <abbyjoydemarest@gmail.com>
2019-05-13 17:05:57 -0400

    almost fully connected the projects page to the nav bar

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,5 +19,6 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.home, name = 'home'),
+    path(' /projects', views.project, name="projects"),
 ]
