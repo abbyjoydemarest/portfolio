cd7279338a60bf78a3c86a59cefa413a16e234f5
Abby Demarest <abbyjoydemarest@gmail.com>
2019-05-14 16:41:49 -0400

    dont open my door

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '98%e!7_ujonvyl)to&^%wh5qc$8i_!bx3fy0w!rgt!^w98wu!('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -126,4 +126,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 django_heroku.settings(locals())
-
